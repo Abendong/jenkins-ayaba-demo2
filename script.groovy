98b8f607beebd9f417fdb188076b647bd2142308
ayaba abendong <sirayaben@gmail.com>
2023-10-15 17:29:58 +0000

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -6,9 +6,9 @@ def buildJar() {
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t abendong/group-pipelin:jma-2.0 .'
+        sh 'docker build -t abendong/group-pipelin:jma-1.2 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push abendong/group-pipelin:jma-2.0'
+        sh 'docker push abendong/group-pipelin:jma-1.2'
     }
 } 
 
